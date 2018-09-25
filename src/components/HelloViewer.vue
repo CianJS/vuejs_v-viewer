73db51cae2fd2bd95a3e26f792c97b73c434ec8a
Erwinousy <erwinousy@gmail.com>
2018-09-25 16:59:33 +0900

    Final v-viewer.

--- src/components/HelloViewer.vue
+++ src/components/HelloViewer.vue
@@ -5,19 +5,20 @@
       <img class="logo" src="../assets/welcome_logo.gif"/>
     </header>
     <hr>
-    <!-- v-viewer를 이용한 Image 생성. -->
+    <!-- img를 v-for로 원하는 개수만큼 불러온 뒤 show() 메서드를 통해 v-viewer에 접근. -->
+    <img class="gallary_image" @click="view(idx)" v-for="(image, idx) in images2" :src="image" :key="image" />
     <viewer :images="images" :options="options" @inited="inited">
-      <img class="gallary_image" v-for="image in images" :src="image" :key="image" />
+      <img v-for="image in images" :src="image" :key="image" class="hide" />
     </viewer>
+    <br>
     Beautiful Pictures!!!
     <br>
     <!-- Methods -->
     <div class="form-group col-ms-12">
-      <button class="btn btn-dark" @click="show(idx)">Show</button>
-      <input type="text" :value="idx" @keyup.enter="show(idx)"/>
+      <button class="btn btn-dark" @click="show()">Show</button>
       <br>
-      <button class="btn btn-dark" @click="view(idx2)">View</button>
-      <input type="text" v-model="idx2" @keyup.enter="view(idx2)"/>
+      <button class="btn btn-dark" @click="view(idx)">View</button>
+      <input type="text" v-model="idx" @keyup.enter="view(idx)"/>
     </div>
 
     <footer>
@@ -44,26 +45,31 @@ export default {
         'https://images.pexels.com/photos/1073567/pexels-photo-1073567.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
         'https://images.pexels.com/photos/35884/amazing-beautiful-beauty-blue.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
       ],
+      images2: [
+        'https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        'https://images.pexels.com/photos/93684/pexels-photo-93684.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        'https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+      ],
       idx: 0,
-      idx2: 2,
       options: {
         title: [1, (i) => `${i.alt.replace(/.jpeg|.jpg|.png/, '')}`],
         movable: false,
         loop: false,
-        initialViewIndex: 2
+        initialViewIndex: 2,
+        fullscreen: false,
         // backdrop: false
       }
     }
   },
   methods: {
     inited(viewer) {
-      this.$viewer = viewer
+      this.$viewer = viewer;
     },
-    show(idx) {
-      this.$viewer.show(idx)
+    show() {
+      this.$viewer.show();
     },
-    view(idx2) {
-      this.$viewer.view(idx2);
+    view(idx) {
+      this.$viewer.view(idx);
     }
   }
 }
@@ -83,6 +89,9 @@ export default {
   display: inline;
   margin: 2px 2px;
   cursor: pointer;
+}
+.hide {
+  display: none;
 }
 header {
   height: 120px;
